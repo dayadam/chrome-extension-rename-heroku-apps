18f731dcf04bcb498ea3fc6924c9d7d6773894da
dayadam <dayadamuga@gmail.com>
2020-02-21 16:32:05 -0500

    cleaned up

--- content.js
+++ content.js
@@ -1,37 +1,14 @@
-//monitorEvents(document.body, 'mouse')
 //document.
-/* (function() {
-  var req = new XMLHttpRequest();
 
-  req.addEventListener("load", function(event) {
-    var src = event.target.responseText;
-    console.log(src);
-  });
-  req.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
-  req.send();
-})(); */
-
-/* window.onload = function() {
-  client = new XMLHttpRequest()
-  XMLHttpRequest.
+  window.onload = function() {
   //window.document.body.onload = doThis;
   //setTimeout(function() {
   //}, 5000);
   //function doThis() {
   console.log("DOM fully loaded and parsed");
-  const apps = document.querySelector(
-    "div.main-content"
-  ).innerText;
   const apps = document.querySelectorAll(".app-list > div > a > div > span");
-  //const apps = document.querySelector("")
   const body = document.getElementsByTagName("body")[0].innerHTML;
-  //const apps = document.getElementsByTagName("a").innerHTML;
-  //reddit
-  const apps = document.querySelector(
-      "#t3_f6py7o > div._1poyrkZ7g36PawDueRza-J > div > div._1Y6dfr4zLlrygH-FLmr8x- > div._2FCtq-QzlfuN-SwVMUZMM3.t3_f6py7o > div.y8HYJ-y_lTUHkQIc1mdCq._2INHSNB8V5eaWp4P0rY_mE > a > div > h3"
-    ).innerText;
-  // > div.app-list > div.apps-list-item > a > div.items-baseline > span
   console.log(apps);
   console.log(body);
   //}
-}; */
+};
