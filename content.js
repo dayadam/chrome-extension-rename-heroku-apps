deaaffa3fe7c5e99d014b3ef7fd88f93e4d713a2
dayadam <dayadamuga@gmail.com>
2020-02-22 02:04:02 -0500

    local storage seems to be working

--- content.js
+++ content.js
@@ -1,5 +1,14 @@
+//get local img url in chrome runtime env
 const pencilImgURL = chrome.runtime.getURL("images/pencil.png");
+// set an empty array if no data in localStorage
+if (localStorage.getItem("apps") === null) {
+  localStorage.setItem("apps", "[]");
+}
+// storage saved in string, must parse/ stringify
+const apps = JSON.parse(localStorage.getItem("apps"));
 
+// heroku is client-side rendered, so listen for DOM mutation
+// in order to see DOM tree. wait till on load for rendering to occur
 window.onload = function() {
   console.log("DOM fully loaded and parsed");
   const body = document.getElementsByTagName("body")[0];
@@ -16,6 +25,31 @@ window.onload = function() {
     for (let mutation of mutationsList) {
       mutation.addedNodes.forEach(addedNode => {
         if (addedNode.className === "f3 near-black") {
+          //setting first local storage / apps if none exist
+          if (apps.length === 0) {
+            apps.push({
+              herokuName: addedNode.textContent.trim(),
+              editedName: ""
+            });
+            localStorage.setItem("apps", JSON.stringify(apps));
+          }
+          // looping through apps to see if app exists in apps / storage,
+          // and if not push it to apps / saving to local storage
+          let found = false;
+          for (i = 0; i < apps.length; i++) {
+            if (apps[i].herokuName === addedNode.textContent.trim()) {
+              found = true;
+              console.log(`${addedNode.textContent.trim()} exists in storage`);
+            } else if (i === apps.length - 1 && found === false) {
+              console.log(`${addedNode.textContent.trim()} pushed to apps`);
+              apps.push({
+                herokuName: addedNode.textContent.trim(),
+                editedName: ""
+              });
+              localStorage.setItem("apps", JSON.stringify(apps));
+            }
+          }
+          //edit div and img
           const imgDiv = document.createElement("div");
           imgDiv.setAttribute("class", "chrome-app-item edit");
           const editImg = document.createElement("img");
@@ -27,6 +61,7 @@ window.onload = function() {
           );
           imgDiv.appendChild(editImg);
           addedNode.nextSibling.nextSibling.appendChild(imgDiv);
+          //delete div and "X" text
           const deleteDiv = document.createElement("div");
           deleteDiv.setAttribute("class", "chrome-app-item delete");
           const deleteDivText = document.createElement("span");
@@ -36,6 +71,8 @@ window.onload = function() {
           );
           deleteDivText.innerText = "X";
           deleteDiv.appendChild(deleteDivText);
+          // prevent anchor tag href redirect with stopPropagation when
+          // edit and delete are clicked
           addedNode.nextSibling.nextSibling.appendChild(deleteDiv);
           deleteDiv.addEventListener("click", function(event) {
             event.stopPropagation();
@@ -54,5 +91,6 @@ window.onload = function() {
   // Create an observer instance linked to the callback function
   const observer = new MutationObserver(callback);
   // Start observing the target node for configured mutations
+  // listening for DOM changes
   observer.observe(body, config);
 };
